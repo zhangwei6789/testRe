f637ea4338e395a23937d55fb335f4e14f2f7dd9
zhangwei <wyzhangwei@126.com>
2021-05-25 22:25:50 +0800

    新建user类

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+/**
+ * @author: zhangwei
+ * @date: 2021/05/25 22:19
+ **/
+public class User {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
